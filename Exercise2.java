2a2737dc6bd416e348859ba3b66e31735f4559a7
Laney Martin <43541394+lbm0015@users.noreply.github.com>
2018-09-24 08:53:34 -0500

    Update Exercise2.java

--- Exercise2.java
+++ Exercise2.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 /*
- *
+ * Programming Assignment2 COMP 3350
+ * Author: Laney Martin
+ * Date: September 20,2018
  */
  
  public class Assignment2 {
- 
+   
+  /** Main method to get input */
    public static void main(String[] args) {
       Scanner scan = new Scanner(System.in);
       System.out.println("Enter a 16-bit binary number: ");
@@ -13,6 +16,7 @@ import java.util.Scanner;
       System.out.println("Value of the binary number: " + binaryString2Decimal(binary));
    }
    
+  /** Converts a binary string to decimal int */
    public static int binaryString2Decimal(String binary) {
       int finalValue = 0;
       char[] cArray = binary.toCharArray();
